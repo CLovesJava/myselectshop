ea45b92dc85bda9567f304a6f1939f8b2c61458d
CLovesJava <wntjdcl1@gmail.com>
2024-11-05 21:35:35 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUser(User user);
 }
