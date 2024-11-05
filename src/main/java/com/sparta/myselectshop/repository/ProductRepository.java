6f654fd0884965af92bd2f72e3021267bbdf204a
CLovesJava <wntjdcl1@gmail.com>
2024-11-05 20:21:39 +0900

    관심상품 등록 API

---
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
